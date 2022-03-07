537d25eab320ca585c9e803c99f075634d18393f
timelessnesses <mooping3roblox@gmail.com>
2022-03-07 13:14:07 +0700

    config

---
+++ config.py
@@ -0,0 +1,5 @@
+hard = True  # Simulate Hard Mode (True/False)
+
+additional_words = []  # Add your own words here (list)
+
+clean = False  # If you want the game to ignore the dictionary (True/False)
